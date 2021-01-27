1b7f7096a255477e8ad8f28e9d2a26b3aca26fcc
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-27 12:44:27 +0100

    FirstEsercizio

--- TypeScriptProject/First/build/index.js
+++ TypeScriptProject/First/build/index.js
@@ -1,2 +1,42 @@
 "use strict";
-console.log("porcoddio");
+class bank {
+    constructor(accounts, transactions) {
+        this.accounts = [];
+        this.transactions = [];
+        this.transactions;
+        this.accounts;
+    }
+    createAccount(countNumber, money) {
+        var user = {
+            countNumber: countNumber,
+            money: money
+        };
+        this.accounts.push(user);
+    }
+    generateTransfer(countSend, countReceives, moneyTransfer) {
+        var transaction = {
+            countSend: countSend,
+            countReceives: countReceives,
+            moneyTransfer: moneyTransfer
+        };
+        this.accounts.filter(account => { if (account.countNumber === countSend) {
+            account.money -= moneyTransfer;
+        } });
+        this.accounts.filter(account => { if (account.countNumber === countReceives) {
+            account.money += moneyTransfer;
+        } });
+        this.transactions.push(transaction);
+    }
+    getAccounts() {
+        return this.accounts;
+    }
+    getTransactions() {
+        return this.transactions;
+    }
+}
+var primabanca = new bank([], []);
+primabanca.createAccount('12345', 1000);
+primabanca.createAccount('10125', 1000);
+primabanca.generateTransfer('12345', '10125', 100);
+console.log("Accounts: ", primabanca.getAccounts());
+console.log('Transaction: ', primabanca.getTransactions());
--- TypeScriptProject/First/src/index.ts
+++ TypeScriptProject/First/src/index.ts
@@ -1,1 +1,49 @@
-console.log("porcoddio")
+class bank{
+    accounts:any[] = []
+    transactions:any[] = []
+
+    constructor(accounts:string[],transactions:number[]){
+        this.transactions 
+        this.accounts 
+    }
+    createAccount(countNumber:string,money:number): any{
+        var user = { 
+            countNumber:countNumber,
+            money:money
+        }
+        this.accounts.push(user)
+    }
+    generateTransfer(countSend:string,countReceives:string,moneyTransfer:number):any{
+        var transaction = {
+            countSend : countSend,
+            countReceives : countReceives,
+            moneyTransfer : moneyTransfer
+        }
+        this.accounts.filter(account=> {if(account.countNumber === countSend){account.money -= moneyTransfer}})
+        this.accounts.filter(account=> {if(account.countNumber === countReceives){account.money += moneyTransfer}})
+        this.transactions.push(transaction)
+    }
+    getAccounts():any{
+        return this.accounts
+    }
+    getTransactions():any{
+        return this.transactions
+    }
+}
+
+
+var primabanca = new bank([],[])
+
+primabanca.createAccount('12345',1000)
+primabanca.createAccount('10125',1000)
+
+primabanca.generateTransfer('12345','10125',100)
+
+console.log("Accounts: ",primabanca.getAccounts())
+console.log('Transaction: ',primabanca.getTransactions())
+
+
+
+
+
+
