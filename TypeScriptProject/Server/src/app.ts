dfa3217733b691102d39a2685e45b00bdf8331de
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-02-01 08:31:02 +0100

    Last

--- TypeScriptProject/Server/src/app.ts
+++ TypeScriptProject/Server/src/app.ts
@@ -28,7 +28,7 @@ app.get('/banks/:branch/accounts', async({params:{branch}},res)=> { //tutti gli 
 })
 app.post('/banks/:branch/accounts',async({params:{branch},body:{namePerson,budget}},res) => { //inserire un nuovo account in una banca OK!
     Bank.generateAccount(branch,namePerson,budget)    
-     await res.json({banks:banks})
+    await res.json({banks:banks})
 })
 app.delete('/banks/:branch/accounts',async({params:{branch},body:{IBAN}},res) => { //eliminare account da una banca OK!
     Bank.deleteAccount(branch,IBAN)
@@ -36,9 +36,9 @@ app.delete('/banks/:branch/accounts',async({params:{branch},body:{IBAN}},res) =>
 })
 app.post('/banks/:branch/transfers',async({params:{branch},body:{IBANsends,IBANreceive,moneyTransfer}},res) => { //trasferimento denaro da due conti della stessa banca OK!
     Bank.generateInternalTransfer(branch,IBANsends,IBANreceive,moneyTransfer)    
-     await res.json(Bank.getAccounts(branch))
+    await res.json(Bank.getAccounts(branch))
 })
 app.post('/banks/:branchsend/transfers/:branchreceive',async({params:{branchsend,branchreceive},body:{IBANsends,IBANreceive,moneyTransfer}},res) => { //trasferimento denaro da due conti di banche diverse OK!
     Bank.generateGlobalTransfer(branchsend,branchreceive,IBANsends,IBANreceive,moneyTransfer)    
-     await res.json({banks:banks})
+    await res.json({banks:banks})
 })
